e55527fde34246f44f3b0fce778a97d91e76128a
George <glthomas526@gmail.com>
2022-12-15 21:53:36 -0500

    added basic city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let searchQuery = document.getElementById("search-bar");
+
 fetch('https://api.openbrewerydb.org/breweries?by_city=Richmond')
   .then((response) => response.json())
   .then((data) => console.log(data));
@@ -9,6 +11,18 @@ function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+}
+
+function searchBreweries(searchValue) {
+  document.getElementById("locations-grabbed").innerHTML = "";
+  fetch('https://api.openbrewerydb.org/breweries?by_city=' + searchValue + "&per_page=10")
+  .then((response) => response.json())
+  .then((data) => {
+    for (i = 0; i < data.length; i++) {
+      document.getElementById("locations-grabbed").innerHTML = document.getElementById("locations-grabbed").innerHTML + ("<br>" + data[i].name + "<br>" + data[i].street + "<br>");
+      console.log(data[i].name);
+    }
+  })
 }
 
 window.initMap = initMap;
