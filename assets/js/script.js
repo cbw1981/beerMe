9da1c99b17f3b36d189b9c8111ebaecd8f5dc267
Cbw1981 <braxtonwilkey@gmail.com>
2022-12-15 20:23:27 -0500

    additional stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 fetch('https://api.openbrewerydb.org/breweries?by_city=Richmond')
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+  let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
