a09ea3fe7f680b042a961e355f893d2d32d4ae43
GeorgeWorkPC <glthomas526@gmail.com>
2022-12-16 09:59:22 -0500

    added styling to nav ui and added state search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let searchQuery = document.getElementById("search-bar");
+let stateValue = '';
 
 fetch('https://api.openbrewerydb.org/breweries?by_city=Richmond')
   .then((response) => response.json())
@@ -17,7 +18,10 @@ console.log(searchQuery.value)
 
 function searchBreweries(searchValue) {
   document.getElementById("locations-grabbed").innerHTML = "";
-  fetch('https://api.openbrewerydb.org/breweries?by_city=' + searchValue + "&per_page=10")
+  if (document.getElementById("state-search-bar").value !== '') {
+    stateValue = "&by_state=" + document.getElementById("state-search-bar").value;
+  }
+  fetch('https://api.openbrewerydb.org/breweries?by_city=' + searchValue + "&per_page=10" + stateValue)
   .then((response) => response.json())
   .then((data) => {
     for (i = 0; i < data.length; i++) {
