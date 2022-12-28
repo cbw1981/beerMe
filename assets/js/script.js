380cb7957305337f3bb53ae60b848bf3a8c8989f
GeorgeWorkPC <glthomas526@gmail.com>
2022-12-28 12:51:53 -0500

    changed css and added search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,69 @@ var storedSearches = [];
 let previousSearchesArray = [];
 let currentSearchLog = -1;
 let searchLogLimit = 5;
+let outputState = '';
 
-if (localStorage.getItem('previousSearches') !== undefined) {
-  if (!(storedSearches.length >= searchLogLimit)) { 
-    searchContainer.innerHTML = searchContainer.innerHTML + `<button class='search-button'>${storedSearches[currentSearchLog]}</button>`; 
+function convertToAbbreviation(stateName) {
+  var states = [
+    ['Arizona', 'AZ'],
+    ['Alabama', 'AL'],
+    ['Alaska', 'AK'],
+    ['Arkansas', 'AR'],
+    ['California', 'CA'],
+    ['Colorado', 'CO'],
+    ['Connecticut', 'CT'],
+    ['Delaware', 'DE'],
+    ['Florida', 'FL'],
+    ['Georgia', 'GA'],
+    ['Hawaii', 'HI'],
+    ['Idaho', 'ID'],
+    ['Illinois', 'IL'],
+    ['Indiana', 'IN'],
+    ['Iowa', 'IA'],
+    ['Kansas', 'KS'],
+    ['Kentucky', 'KY'],
+    ['Louisiana', 'LA'],
+    ['Maine', 'ME'],
+    ['Maryland', 'MD'],
+    ['Massachusetts', 'MA'],
+    ['Michigan', 'MI'],
+    ['Minnesota', 'MN'],
+    ['Mississippi', 'MS'],
+    ['Missouri', 'MO'],
+    ['Montana', 'MT'],
+    ['Nebraska', 'NE'],
+    ['Nevada', 'NV'],
+    ['New Hampshire', 'NH'],
+    ['New Jersey', 'NJ'],
+    ['New Mexico', 'NM'],
+    ['New York', 'NY'],
+    ['North Carolina', 'NC'],
+    ['North Dakota', 'ND'],
+    ['Ohio', 'OH'],
+    ['Oklahoma', 'OK'],
+    ['Oregon', 'OR'],
+    ['Pennsylvania', 'PA'],
+    ['Rhode Island', 'RI'],
+    ['South Carolina', 'SC'],
+    ['South Dakota', 'SD'],
+    ['Tennessee', 'TN'],
+    ['Texas', 'TX'],
+    ['Utah', 'UT'],
+    ['Vermont', 'VT'],
+    ['Virginia', 'VA'],
+    ['Washington', 'WA'],
+    ['West Virginia', 'WV'],
+    ['Wisconsin', 'WI'],
+    ['Wyoming', 'WY'],
+];
+
+stateName = stateName.toUpperCase();
+for(i = 0; i < states.length; i++) {
+    if(states[i][1] == stateName) {
+      outputState = states[i][0];
+      return(outputState);
+    }
   }
-  console.log(currentSearchLog);
 }
 
 function getTotalBreweries() {
@@ -43,18 +100,24 @@ function incrementPage(amount) {
   if (amount == -1) {
     if (currentPage !== 1) {
       currentPage = currentPage + amount;
-      searchBreweries(document.getElementById('city-search-bar').value, currentPage);
+      searchBreweries(document.getElementById('city-search-bar').value, currentPage, false);
     }
   }
   if (amount == 1) {
     if (currentPage !== totalPages) {
       currentPage = currentPage + amount;
-      searchBreweries(document.getElementById('city-search-bar').value, currentPage);
+      searchBreweries(document.getElementById('city-search-bar').value, currentPage, false);
     }
   }
 }
 
-function searchBreweries(searchValue, page) {
+function replaceSearch(city, state) {
+  document.getElementById("city-search-bar").value = city;
+  document.getElementById("state-search-bar").value = state;
+  searchBreweries(document.getElementById('city-search-bar').value, 1, false)
+}
+
+function searchBreweries(searchValue, page, store) {
   
   
   console.log(previousSearchesArray);
@@ -69,23 +132,61 @@ function searchBreweries(searchValue, page) {
     stateValue = "&by_state=" + document.getElementById("state-search-bar").value.replace(/ /g, '+');
   }
   storedLocation = cityReplaced + stateValue;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer sp-mvayoOWfVv8Xu3Z9Pk_07izScmov58heqpF3fg7BCd2KfaxNVzbe4whtbzvzTbJnmzL2XNBrvM9xvcGGD2Ko1HJBvRlLdq2UXtS84-IrvGGfPvY68aFqzXkSsY3Yx'
+    }
+  };
+  
+
+
   fetch('https://api.openbrewerydb.org/breweries' + cityReplaced + "&per_page=10" + stateValue + `&page=${page}`)
   .then((response) => response.json())
   .then((data) => {
+    if (store == true) {
+      if (currentSearchLog < searchLogLimit) {
+          previousSearchesArray.push([cityName = searchValue, stateName = document.getElementById("state-search-bar").value])
+          currentSearchLog = currentSearchLog + 1;
+        }
+        else {
+          previousSearchesArray.shift();
+          previousSearchesArray.push([cityName = searchValue, stateName = document.getElementById("state-search-bar").value])
+          currentSearchLog = currentSearchLog + 1;
+        }
+        
+      localStorage.setItem('previousSearches', JSON.stringify(previousSearchesArray));
+      console.log("previoussearchesarray " + previousSearchesArray);
+      storedSearches = JSON.parse(localStorage.getItem("previousSearches"));
+      console.log(storedSearches + "storedsearches");
+      console.log(currentSearchLog + "log1");
+
+      if (localStorage.getItem('previousSearches') !== undefined) {
+        if (!(storedSearches.length == searchLogLimit)) {
+            if (stateValue !== undefined) {
+              searchContainer.innerHTML = searchContainer.innerHTML + `<button class='search-button m-2 p-1' onclick='replaceSearch("${storedSearches[currentSearchLog][0]}", "${storedSearches[currentSearchLog][1]}");'>${storedSearches[currentSearchLog][0] + ", " + storedSearches[currentSearchLog][1]}</button>`;
+            } 
+        }
+        console.log(currentSearchLog);
+      }
+    }
+
     for (i = 0; i < data.length; i++) {
       locationContainer = document.createElement('section');
       locationContainer.setAttribute("id", "inner-location-container");
-      locationContainer.setAttribute("class", "bg-orange-400 relative grid grid-cols-7 p-0 mt-10 rounded-lg grid-rows-1");
+      locationContainer.setAttribute("class", "bg-zinc-800 relative grid grid-cols-7 p-0 mt-10 rounded-lg grid-rows-1");
       leftColumn = document.createElement('section');
       leftColumn.setAttribute("id", "left-column");
       leftColumn.setAttribute("class", "md:col-span-2 col-span-7");
       rightColumn = document.createElement('section');
       rightColumn.setAttribute("id", "right-column");
-      rightColumn.setAttribute("class", "md:col-span-4 col-span-7 pl-4 pt-[10px]");
-      if (data[i].street != null) {
+      rightColumn.setAttribute("class", "md:col-span-4 col-span-7 pt-[10px]");
+      if (data[i].street !== undefined) {
         let spacedStreet = data[i].street.replace(/ /g, '+');;
         leftColumn.innerHTML = leftColumn.innerHTML + (`<iframe
-        class="rounded-lg rounded-r-none w-full h-full"
+        class="rounded-lg md:rounded-r-none md:rounded-b-none max-md:rounded-b-none w-full h-full"
         id="google-map min-h-[250px]"
         width="200"
         height="250"
@@ -94,15 +195,15 @@ function searchBreweries(searchValue, page) {
         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAHXC5hqdUUZf7FwQ_DnsfJ09qlk3xbick&q=${spacedStreet},${data[i].city},${data[i].state}">
       </iframe>`)
       } else {
-        leftColumn.innerHTML = leftColumn.innerHTML + `<div class="h-[250px] w-full bg-gray-700 rounded-lg rounded-r-none relative"><p class="absolute top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2">No map available.</p></div>`
+        leftColumn.innerHTML = leftColumn.innerHTML + `<div class="h-[250px] w-full bg-gray-700 max-md:rounded-t-lg md:rounded-l-lg rounded-r-none relative"><p class="absolute top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2">No map available.</p></div>`
       }
-      rightColumn.innerHTML = rightColumn.innerHTML + ("<h1 class='text-2xl text-left'>" + data[i].name + "</h1>");
+      rightColumn.innerHTML = rightColumn.innerHTML + ("<h1 class='text-2xl text-left pl-4'>" + data[i].name + "</h1>");
       if (data[i].street !== null) {
-        rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600'>" + data[i].street + "<h3>");
+        rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600 pl-4'>" + data[i].street + "<h3>");
       } else {
-        rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600'>No street found.</h3>");
+        rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600 pl-4'>No street found.</h3>");
       }
-      rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600 mb-10'>" + data[i].city + "<h3>");
+      rightColumn.innerHTML = rightColumn.innerHTML + ("<h3 class='text-left text-sm text-gray-600 mb-10 pl-4'>" + data[i].city + "<h3>");
       let capitalizedState = data[i].state[0].toUpperCase() + data[i].state.substring(1);
       rightColumn.innerHTML = rightColumn.innerHTML + (`<div id='bar-tag' class='text-left inline md:absolute md:right-2 md:top-[10px] border-solid rounded-[20px] border-2 p-2 max-md:mr-2 ml-2 w-fit'><span class="material-symbols-outlined text-sm">
       sell
@@ -113,26 +214,15 @@ function searchBreweries(searchValue, page) {
         sell
         </span> ${capitalizedType}</div>`);
         }
-        if (data[i].website_url !== null) {
-        rightColumn.innerHTML = rightColumn.innerHTML + (`<br><button class='align-bottom border-solid rounded-lg border-2 p-2 mt-4 mb-4'><a href='${data[i].website_url}' target="_blank" rel="noopener noreferrer">Website</button>`);
-        }
-        if (data[i].website_url !== null && data[i].phone !== null) {
-          rightColumn.innerHTML = rightColumn.innerHTML + (`<div id='button-divider' class='inline ml-2 mr-2'></div>`);
-        }
-        if (data[i].phone == null) {
-          rightColumn.innerHTML = rightColumn.innerHTML + "";
-        }
-        if (data[i].phone !== null && data[i].website_url !== null) {
-          rightColumn.innerHTML = rightColumn.innerHTML + (`<button class='align-bottom border-solid rounded-lg border-2 p-2 mb-4'><a href='tel:${data[i].phone}'>Call</button><br>`);
-        }
-        if (data[i].phone !== null && data[i].website_url == null) {
-          rightColumn.innerHTML = rightColumn.innerHTML + (`<button class='align-bottom border-solid rounded-lg border-2 p-2 mb-4'><a href='tel:${data[i].phone}'>Call</button><br>`);
-        }
+
+      
+        
 
       document.getElementById("previous-searches-container").appendChild(searchContainer);
       locationsGrabbed.appendChild(locationContainer);
       locationContainer.appendChild(leftColumn);
       locationContainer.appendChild(rightColumn);
+      locationContainer.innerHTML = locationContainer.innerHTML + `<br class='max-md:hidden'><button id="more-info" class="w-[700%] ml-0 max-md:mt-8 p-4 bg-red-600 rounded-b-lg">More Info.</div>`;
       
       console.log(data[i].name);
     }
@@ -295,19 +385,6 @@ function convertToFullState() {
   
   searchContainer = document.createElement('section');
   searchContainer.setAttribute("id", "inner-search-container");
-  if (currentSearchLog < searchLogLimit) {
-  previousSearchesArray.push([cityName = document.getElementById('city-search-bar').value, stateName = rawStateValue.value])
-  currentSearchLog = currentSearchLog + 1;
-  }
-    
-  localStorage.setItem('previousSearches', JSON.stringify(previousSearchesArray));
-  console.log("previoussearchesarray " + previousSearchesArray);
-  storedSearches = JSON.parse(localStorage.getItem("previousSearches"));
-  console.log(storedSearches + "storedsearches");
-  console.log(currentSearchLog + "log1");
 
-  if (localStorage.getItem('previousSearches') !== undefined) {
-    searchContainer.innerHTML = searchContainer.innerHTML + storedSearches[currentSearchLog];
-    console.log(storedSearches + "log1");
-  }
+    
 }
